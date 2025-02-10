1ed078ddf8b60b0e868522210bd9979ddeb9fa35
zotel1 <zotelsigel@gmail.com>
2025-02-10 17:38:39 -0300

    26 mins agregamos rutas y le agregamos estilos a nuestro home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent)
+    },
+    {
+        path: 'profile',
+        loadComponent: () => import('./pages/profile/profile.component').then(c => c.ProfileComponent)
+    },
+    {
+        path: 'calendar',
+        loadComponent: () => import('./pages/calendar/calendar.component').then(c => c.CalendarComponent)
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button'
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [ MatButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
