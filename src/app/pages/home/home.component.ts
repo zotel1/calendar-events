68f8ef0d7932d998f2117a47d81e6df15675d547
zotel1 <zotelsigel@gmail.com>
2025-02-10 18:10:36 -0300

    adaptamos nuestro app component y el home y el header

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatButtonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button'
+import { RouterModule } from '@angular/router';
+import { AuthService } from '@auth0/auth0-angular'
 
 @Component({
   selector: 'app-header',
-  imports: [ MatButtonModule],
+  imports: [ MatButtonModule, RouterModule ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  constructor(
+    public authService: AuthService
+  ){}
 }
