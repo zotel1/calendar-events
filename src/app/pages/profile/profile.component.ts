51726123dd206bada9c3e95f7161350d480c16c2
zotel1 <zotelsigel@gmail.com>
2025-02-11 01:16:17 -0300

    editamos nuestra ruta de profile min 46

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { AuthService, User } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [MatButtonModule, MatCardModule, MatProgressSpinnerModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
+  
+  user: User | null | undefined;
+  isLoaded = false;
+  
+  constructor(public authService: AuthService ) {
+    this.authService.user$.subscribe(user => {
+      this.user = user;
+      this.isLoaded = true;
+  });
+  }
 
 }
