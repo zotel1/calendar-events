e64a1cee86147bfff1be543b0a57f80e798fefe4
zotel1 <zotelsigel@gmail.com>
2025-02-12 21:46:05 -0300

    le vamos dandop forma al html del calendar

--- src/app/pages/calendar/calendar.component.ts
+++ src/app/pages/calendar/calendar.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-calendar',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss'
 })
 export class CalendarComponent {
+
+  openModal(){
+    
+  }
 
 }
