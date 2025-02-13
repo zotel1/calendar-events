1eb32e045265e8ede2abe52017a625f0257167d5
zotel1 <zotelsigel@gmail.com>
2025-02-12 21:19:05 -0300

    aseguramos nuestras rutas en app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     {
@@ -7,10 +8,12 @@ export const routes: Routes = [
     },
     {
         path: 'profile',
+        canActivate: [authGuard],
         loadComponent: () => import('./pages/profile/profile.component').then(c => c.ProfileComponent)
     },
     {
         path: 'calendar',
+        canActivate: [authGuard],
         loadComponent: () => import('./pages/calendar/calendar.component').then(c => c.CalendarComponent)
     },
     {
