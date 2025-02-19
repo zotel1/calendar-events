bbefbaccaf6d1b3f4fb5523b36fc3d3c1c3f1db0
zotel1 <zotelsigel@gmail.com>
2025-02-18 23:19:51 -0300

    empezamos a agregar las importaciones con ls que vamos a trabajar en nuestro dialog component

--- src/app/shared/dialog/dialog.component.ts
+++ src/app/shared/dialog/dialog.component.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { NgxColorsModule } from 'ngx-colors';
+
+import {
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+  MatDialogContent,
+  MatDialogActions,
+  MatDialogClose,
+} from '@angular/material/dialog';
+
+import { MatButtonModule } from '@angular/material/button';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-dialog',
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+  ],
   templateUrl: './dialog.component.html',
   styleUrl: './dialog.component.scss'
 })
